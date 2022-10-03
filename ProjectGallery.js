e3c391209c10f44806cb2ac8493002e6db694809
Taha Hassan <tahafhassan@gmai.com>
2022-10-03 17:28:23 +0100

    one Button hides content from one div

---
+++ ProjectGallery.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var text = document.getElementById("animation");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
