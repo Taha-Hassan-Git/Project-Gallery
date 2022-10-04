738d983b8bc1c07f507604371d9b105c7d11eb64
Taha Hassan <tahafhassan@gmai.com>
2022-10-04 17:46:52 +0100

    Button now toggles Hide/show

--- ProjectGallery.js
+++ ProjectGallery.js
@@ -3,7 +3,9 @@ function myFunction(button) {
     var myDiv = document.getElementById(text);
     if (myDiv.style.display === "none") {
       myDiv.style.display = "block";
+      button.innerHTML = "Hide"
     } else {
       myDiv.style.display = "none";
+      button.innerHTML = "Show"
     }
 }
